6eb81a7ee1d568a4406546a815882ff10a5e24b1
Pascoooo <162371632+Pascoooo@users.noreply.github.com>
2025-07-11 17:47:59 +0200

    added triviagame and starting the logic

--- src/main/java/org/it/uniba/fox/InteractionManager/UserInputFlow.java
+++ src/main/java/org/it/uniba/fox/InteractionManager/UserInputFlow.java
@@ -14,6 +14,11 @@ public class UserInputFlow {
      * The constant Event that represents the current event.
      */
     public static int Event;
+
+    /**
+     * The triviaGame object that manages the trivia game in case 2.
+     */
+    private static TriviaGame triviaGame;
     /**
      * The parser object that parses the user input in case 0.
      */
@@ -23,6 +28,21 @@ public class UserInputFlow {
 
     public static void gameFlow(final String text) {
         OutputDisplayManager.displayText(text);
+
+        switch (Event) {
+            case 0: // Command parsing and execution
+                parserFlow(text);
+                break;
+            case 1: // Trivia game
+                triviaFlow(text);
+                // triviaGame.handleInput(text);
+                break;
+            case 2: // Hangman game
+                // hangmanGame.handleInput(text);
+                break;
+            default:
+                OutputDisplayManager.displayText("Comando non riconosciuto.");
+        }
     }
 
     /**
@@ -36,7 +56,24 @@ public class UserInputFlow {
 
     public static void setUpGameFlow(final Game game) {
         OutputDisplayManager.displayText("Benvenuto nel gioco! Digita un comando per iniziare.");
+        Event = 1;
     }
 
+
+    /**
+     * The method to manage the trivia game event.
+     *
+     * @param text the user input
+     */
+    public static void triviaFlow(final String text) {
+        triviaGame = TriviaGame.getInstance();
+
+        try {
+            triviaGame.checkGuess(text);
+            if (Event == 1) triviaGame.getQAndA();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/org/it/uniba/fox/Logic/GameLogic.java
+++ src/main/java/org/it/uniba/fox/Logic/GameLogic.java
@@ -1,5 +1,46 @@
 package org.it.uniba.fox.Logic;
 
-public class GameLogic {
+import org.it.uniba.fox.Entity.Character;
+import org.it.uniba.fox.Entity.Game;
+import org.it.uniba.fox.Entity.Room;
+import org.it.uniba.fox.GUI.GameGUI;
+import org.it.uniba.fox.InteractionManager.UserInputFlow;
 
+/**
+ * The class that manages the game logic.
+ */
+public class GameLogic {
+    /**
+     * The game instance.
+     */
+    private Game game;
+    /**
+     * The instance of the game manager.
+     */
+    private final GameManager gameManager = new GameManager();
+
+    /**
+     * Constructor of the class.
+     *
+     * @param game the game instance
+     */
+    public GameLogic(Game game) {
+        this.game = game;
+    }
+
+
+    /**
+     * The actions to perform when the player talks to a personage.
+     *
+     * @param c the personage to talk to
+     * @return true if the action is performed, false otherwise
+     */
+    public boolean talkToPersonage(Character c) {
+        if(c.getPosition().toString().equals("Stanza5")) {
+            UserInputFlow.Event = 1;
+            return true;
+        } else {
+            GameGUI.displayTextPaneSetText("> " + c.getName() + " non è qui!");
+            return false;
+        }
 }
